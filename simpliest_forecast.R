3ac51e92795502008a56e7f9ead2669c4dd5cbef
albertsalgueda <albert.salgueda01@estudiant.upf.edu>
2021-11-03 19:52:59 -0400

    Major Updates: -Integration with Kraken API to update BTC price -Minor improvements on the model -Refactoring

--- simpliest_forecast.R
+++ simpliest_forecast.R
@@ -3,30 +3,30 @@
 
 library("prophet")
 
-market_price <- read_csv("market-price - market-price.csv", 
-                          col_types = cols(ds = col_date(format = "%d/%m/%Y"), 
-                           y = col_number()))
-
+market_price <- read_csv("market_price.csv", 
+                         col_types = cols(ds = col_date(format = "%m/%d/%Y"), 
+                                          y = col_number()))
 market_price <- market_price %>% 
   filter(year(ds)>2016) 
-market_price$ds <- format(as.Date(market_price$ds), "%d/%m/%Y")
-  
+market_price$y <- as.numeric(market_price$y)
+
 #Update and transform data using the API
+#while (TRUE) {
 current_price= market.api.process('bitstamp',c('BTC','USD'),'ticker')
 current_price = subset(current_price,select=c("timestamp","last"))
-current_price$timestamp <- format(as.Date(current_price$timestamp), "%d/%m/%Y")
+current_price$timestamp <- format(as.Date(current_price$timestamp), "%Y-%m-%d")
 current_price <- current_price %>% 
   rename(ds=timestamp,y=last)
-view(current_price)
+current_price$y=as.numeric(current_price$y)
+#}
 
 #merge ( i did it all... but still does not merge properly)
-merge(market_price,current_price) #error
+market_price <- rbind(market_price,current_price)
 view(market_price)
-
 #fit the model using prophet
 Model1 <- prophet(market_price)
 #create future database
-Future1 <- make_future_dataframe(Model1,periods = 365)
+Future1 <- make_future_dataframe(Model1,periods = 100)
 tail(Future1)
 #Forecasting
 forecast1 <- predict(Model1,Future1)
@@ -42,7 +42,18 @@ ratio2 = forecast1$yhat/market_price$y
 
 ##API ANSWER
 #test the API with this simple forecast
+save(Model1,file="prophet.RData")
+saveRDS(Model1, "model.rds")
 
+#LAGGED PREDICTION FROM PRICE OF PRODUCTION
+
+df1 <- market_price
+df1 <- df1 %>%
+  mutate( ProductionLag = lag( "ProductionCost", 30 ), TrendsLagged = lag("Google Trends", 30) )
+view(df1)
+rec <-
+  recipe(df1) %>% 
+  update_role()
 
 
 
